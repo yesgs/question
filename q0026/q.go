62b5ca68a4e001bc8cfc224ba3d675bf6b1db654
king <yewei@myhayo.com>
2022-06-08 00:51:56 +0800

    q0026

---
+++ q0026/q.go
@@ -0,0 +1,18 @@
+package q0026
+
+func removeDuplicates(nums []int) int {
+
+	length := len(nums)
+	if length == 0 {
+		return 0
+	}
+	i := 0
+	for j := 1; j < length; j++ {
+		if nums[j] != nums[i] {
+			i++
+			nums[i] = nums[j]
+		}
+	}
+
+	return i + 1
+}
---
+++ q0026/q_test.go
@@ -0,0 +1,26 @@
+package q0026
+
+import (
+	"fmt"
+	"question/common"
+	"reflect"
+	"testing"
+)
+
+func TestRemoveDuplicates(t *testing.T) {
+
+	var inputs []common.InputIntSliceOutputInt
+	inputs = append(inputs, common.InputIntSliceOutputInt{Input: []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}, Output: 5})
+	inputs = append(inputs, common.InputIntSliceOutputInt{Input: []int{1, 1, 1, 1, 1, 2, 2, 3, 3}, Output: 3})
+
+	for _, input := range inputs {
+		ret := removeDuplicates(input.Input)
+		msg := fmt.Sprintf("输入 %v 期望 %v 实际 %v", input.Input, input.Output, ret)
+		if !reflect.DeepEqual(ret, input.Output) {
+			t.Error("[FAIL]" + msg)
+		} else {
+			t.Log("[OK]" + msg)
+		}
+	}
+
+}
